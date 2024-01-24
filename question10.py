f30ba35d03c4d6d6f47a024e7847c26f28d97cf9
kandelmilan <156769005+kandelmilan@users.noreply.github.com>
2024-01-24 12:50:47 +0545

    updated

--- question10.py
+++ question10.py
@@ -5,13 +5,12 @@
  """
 
 
-number = int(input("Enter the maximum number in the Fibonacci sequence: "))
-print("Invalid input. Please enter a valid number.")
+max_number = int(input("Enter the maximum number in the Fibonacci sequence: "))
+fibonacci_sequence = [0, 1]
+
+while fibonacci_sequence[-1] + fibonacci_sequence[-2] <= max_number:
+    next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+    fibonacci_sequence.append(next_number)
 
 
-fibonacci_number = [0, 1]
-
-
-# not completedz
-
-
+print("Fibonacci sequence up to", max_number, ":", fibonacci_sequence)
